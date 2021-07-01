9329c961c2ae285c6194b003ae5c6952d47c18aa
vksoniya <vk.soniya@gmail.com>
2021-07-01 23:11:59 +0200

    update activate script

--- activatevenv.sh
+++ activatevenv.sh
@@ -3,6 +3,12 @@
 python3 -m venv srvEnv
 source ./srvEnv/bin/activate
 
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
 
 #python3 -m spacy download en_core_web_lg
+
+# Uncomment this code in case by default your environment is not using python 3.8
+#conda create -n py38 python=3.8 
+pip3 install -r requirements.txt
+python3 -m spacy download en_core_web_lg
+python3 srvFakeNewsDetection.py
