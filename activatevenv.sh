bda607199963ebfc80a34e8bd321e4573026ecfc
Soniya Vijayakumar <44982216+vksoniya@users.noreply.github.com>
2022-07-04 15:23:30 +0200

    Update activatevenv.sh
    changed the conda py38 env to actual env srvEnv

--- activatevenv.sh
+++ activatevenv.sh
@@ -7,8 +7,9 @@ source ./srvEnv/bin/activate
 
 #python3 -m spacy download en_core_web_lg
 
-# Uncomment this code in case by default your environment is not using python 3.8
-#conda create -n py38 python=3.8 
+# Uncomment this code in case by default your environment is not using python 3.8 
+#conda create -n srvEnv python=3.8
+
 pip3 install -r requirements.txt
 python3 -m spacy download en_core_web_lg
 python3 srvFakeNewsDetection.py
